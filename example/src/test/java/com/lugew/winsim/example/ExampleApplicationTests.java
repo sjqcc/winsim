8bc9a56976b4c60ab8fac5aa7fae3349d80e0fef
LuGew <lugew@outlook.com>
2020-07-29 15:07:58 +0800

    1. add example

---
+++ example/src/test/java/com/lugew/winsim/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugew.winsim.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
