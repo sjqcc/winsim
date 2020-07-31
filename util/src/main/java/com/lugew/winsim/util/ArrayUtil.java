bf8a41ebd2f31595339d3efefce1ef1c89b9bac6
LuGew <lugew@outlook.com>
2020-07-31 18:20:44 +0800

    1. add valid specification

---
+++ util/src/main/java/com/lugew/winsim/util/ArrayUtil.java
@@ -0,0 +1,21 @@
+package com.lugew.winsim.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author LuGew
+ * @since 2020/7/31
+ */
+@UtilityClass
+public class ArrayUtil {
+
+    public boolean isEmpty(Object[] objects) {
+        return null != objects && objects.length > 0;
+    }
+
+    public boolean isNotEmpty(Object[] objects) {
+        return !isEmpty(objects);
+    }
+
+
+}
